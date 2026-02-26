a77b1c0fc877b201aaa4cb8a489638335622a022
elblasy9 <bodaelblasy44@gmail.com>
2026-02-27 01:26:18 +0200

    admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,27 @@
+function addStudent(){
 
+const name = name.value;
+const email = email.value;
+const phone = phone.value;
+const grade = grade.value;
+const password = password.value;
+
+auth.createUserWithEmailAndPassword(email,password)
+.then(userCredential=>{
+
+const uid = userCredential.user.uid;
+
+db.collection("students").doc(uid).set({
+
+ name: name,
+ email: email,
+ phone: phone,
+ grade: grade
+
+});
+
+alert("تم إضافة الطالب");
+
+});
+
+}
