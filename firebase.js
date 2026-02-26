243344074369fdf0c5591a198c3e44a190b26695
elblasy9 <bodaelblasy44@gmail.com>
2026-02-27 01:20:35 +0200

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDbgJm0fw_dNJbon-Isc00sbXWUse2H1UE",
+    authDomain: "body-6b87f.firebaseapp.com",
+    projectId: "body-6b87f",
+    storageBucket: "body-6b87f.firebasestorage.app",
+    messagingSenderId: "133952292955",
+    appId: "1:133952292955:web:876f2b2eade919dfded77e"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
