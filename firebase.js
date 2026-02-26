e5d92022d5171223c23551756046ff71dfc52490
elblasy9 <bodaelblasy44@gmail.com>
2026-02-27 01:40:58 +0200

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,4 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyDbgJm0fw_dNJbon-Isc00sbXWUse2H1UE",
     authDomain: "body-6b87f.firebaseapp.com",
     projectId: "body-6b87f",
